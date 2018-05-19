a913764c0e58272c070bf21beb99769dd680c4f2
Dmitry Baryshnikov <bishop.dev@gmail.com>
2018-05-19 23:45:17 +0300

    Remove appveyor files

--- autogen.sh
+++ autogen.sh
@@ -29,20 +29,20 @@ fi
 
 # Check that gmock is present.  Usually it is already there since the
 # directory is set up as an SVN external.
-if test ! -e gmock; then
-  echo "Google Mock not present.  Fetching gmock-1.7.0 from the web..."
-  curl $curlopts -L -O https://github.com/google/googlemock/archive/release-1.7.0.zip
-  unzip -q release-1.7.0.zip
-  rm release-1.7.0.zip
-  mv googlemock-release-1.7.0 gmock
-fi
-
-if test ! -e gmock/gtest; then
-  curl $curlopts -L -O https://github.com/google/googletest/archive/release-1.7.0.zip
-  unzip -q release-1.7.0.zip
-  rm release-1.7.0.zip
-  mv googletest-release-1.7.0 gmock/gtest
-fi
+# if test ! -e gmock; then
+#   echo "Google Mock not present.  Fetching gmock-1.7.0 from the web..."
+#   curl $curlopts -L -O https://github.com/google/googlemock/archive/release-1.7.0.zip
+#   unzip -q release-1.7.0.zip
+#   rm release-1.7.0.zip
+#   mv googlemock-release-1.7.0 gmock
+# fi
+#
+# if test ! -e gmock/gtest; then
+#   curl $curlopts -L -O https://github.com/google/googletest/archive/release-1.7.0.zip
+#   unzip -q release-1.7.0.zip
+#   rm release-1.7.0.zip
+#   mv googletest-release-1.7.0 gmock/gtest
+# fi
 
 set -ex
 
